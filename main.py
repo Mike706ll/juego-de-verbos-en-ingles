f2aea49a5365a80b751369f89d799589c09f2b97
miguel <smiguel706@gmail.com>
2020-06-27 15:25:31 -0500

    i have change the contructor of Verb and added a count of errors when trying toread verbs.txt

--- main.py
+++ main.py
@@ -2,14 +2,14 @@ import json
 
 class Verb():
     """ """
-    def __init__(self, regular, simple_form, third_form, ing_form, past_form, participle_form):
+    def __init__(self, regular, forms):
         """ """
         self.regular = regular
-        self.simple_form = simple_form
-        self.third_form = third_form
-        self.ing_form = ing_form
-        self.past_form = past_form
-        self.participle_form = participle_form
+        self.simple_form = forms[0]
+        self.third_form = forms[1]
+        self.ing_form = forms[2]
+        self.past_form = forms[3]
+        self.participle_form = forms[4]
 
     def __str__(self):
         return self.simple_form + '-' + ('regular' if self.regular else 'irregular')
@@ -29,18 +29,25 @@ class ListVerbs():
         with open(file_name) as file_verbs:
             lines = file_verbs.readlines()
         
+        errors = 0
         for line in lines:
             attributes = line.strip().split()
-            regular = True if attributes[0] == 'si' else False
-            verb = Verb(regular, attributes[1], attributes[2], attributes[3],
-                attributes[4], attributes[5])
-            self.add_verb(verb)
-
+            if len(attributes) == 6:
+                regular = True if attributes[0] == 'si' else False
+                verb = Verb(regular, attributes[1:])
+                self.add_verb(verb)
+            else:
+                errors += 1
+            
+        return errors
 
 
 if __name__ == '__main__':
     my_list = ListVerbs()
-    my_list.read_verbs('verbs.txt')
+    total_errors = my_list.read_verbs('verbs.txt')
 
     for verb in my_list.list:
         print(verb)
+
+    print('\n\nTotal Errors : ' + str(total_errors))
+    print('\nVerbs registered : ' + str(len(my_list.list)))
