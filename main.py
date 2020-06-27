04769c6a6b0ee6acb330d35e0eb7a5fdbec67c38
miguel <smiguel706@gmail.com>
2020-06-27 16:33:34 -0500

    Game class created

--- main.py
+++ main.py
@@ -1,18 +1,20 @@
-import json
+from collections import OrderedDict
+from random import sample, choice
 
 class Verb():
     """ """
     def __init__(self, regular, forms):
         """ """
         self.regular = regular
-        self.simple_form = forms[0]
-        self.third_form = forms[1]
-        self.ing_form = forms[2]
-        self.past_form = forms[3]
-        self.participle_form = forms[4]
+        self.conjuction = OrderedDict()
+        self.conjuction['simple'] = forms[0]
+        self.conjuction['third'] = forms[1]
+        self.conjuction['ing'] = forms[2]
+        self.conjuction['past'] = forms[3]
+        self.conjuction['participle'] = forms[4]
 
     def __str__(self):
-        return self.simple_form + '-' + ('regular' if self.regular else 'irregular')
+        return self.conjuction['simple'] + '-' + ('regular' if self.regular else 'irregular')
     
 class ListVerbs():
     """ """
@@ -41,13 +43,40 @@ class ListVerbs():
             
         return errors
 
+class Game():
+    """ """
+
+    def __init__(self):
+        """"""
+        self.list_verbs = ListVerbs()
+        errors = self.list_verbs.read_verbs('verbs.txt')
+        if errors == 0:
+            print('Verbs read succesfully')
+        else:
+            print('Please check verbs list for a better game')
+    
+    def random_quiz(self):
+        questions = ['third', 'ing', 'past', 'participle']
+        number_questions = int(input('How many questions do yo want to do? '))
+        if number_questions > len(self.list_verbs.list) or number_questions <= 0:
+            number_questions = 10
+        
+        correct_answers = 0
+        verbs_quiz = sample(self.list_verbs.list, number_questions)
+
+        for verb in verbs_quiz:
+            question = choice(questions)
+            msg = verb.conjuction['simple'] + ' in ' + question + ' form: '
+            answer = input(msg)
+            if answer == verb.conjuction[question]:
+                correct_answers += 1
+                print('good')
+            else:
+                print('bad')
+        
+        print('You have ' + str(correct_answers) + ' correct answers of ' + str(number_questions))
 
 if __name__ == '__main__':
-    my_list = ListVerbs()
-    total_errors = my_list.read_verbs('verbs.txt')
+    new_game = Game()
+    new_game.random_quiz()
 
-    for verb in my_list.list:
-        print(verb)
-
-    print('\n\nTotal Errors : ' + str(total_errors))
-    print('\nVerbs registered : ' + str(len(my_list.list)))
